63c36827c5a79fd4de4594b9b5b753f6ff4d74a7
SUMIT PARKAR <sumitparkar1802@gmail.com>
2026-08-24 23:23:23 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,2 +1,28 @@
 
 print ("Hello World")
+
+
+print("Student Grade-Card")
+dms = float(input("Enter your marks of DMS : "))
+se = float(input("Enter your marks of SOFTWARE ENGINEERING : "))
+cn = float(input("Enter your marks of COMPUTER NETWORK  : "))
+dbms = float(input("Enter your marks of DATABASE MANAGEMENT SYSTEM  : "))
+cc = float(input("Enter your marks of CLOUD COMPUTING : "))
+
+total = (dms + se + cn + dbms +cc ) 
+print("Total is : ",total)
+avg = (total )/ 5
+
+score = int (avg)
+print("Score is : ",score)
+
+if (score>=90 and score<=100):
+    print("Your Grade is : A")
+elif(score>=80 and score<=89):
+    print("Your Grade is : B")
+elif(score>=70 and score<=79):
+    print("Your Grade is : C")
+elif(score>=60 and score<=69):
+    print("Your Grade is : D")
+else:
+    print("Your Grade is : F")
